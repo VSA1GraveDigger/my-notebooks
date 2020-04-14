b1b9a45a0702e1c2971e427e092ade0a732bb6f7
Jay Gambetta <jay.gambetta@us.ibm.com>
2020-04-13 20:47:50 -0400

    cleaning up

--- Qconfig_IBMQ_experience_staging.py
+++
@@ -1,3 +0,0 @@
-APItoken = "3fc3fec31eb2bacfccfa64d781979b307b9b3f7bf4a121aa9340e8cad5a19bcb3c775e3465ef7a2054b103b3f2a10f9d12d4846a6c6c16655621de750dca0a6d"
-
-url = "https://qcwi-develop.mybluemix.net/api"
