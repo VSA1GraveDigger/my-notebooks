75551120cb9c103ad945798605a2fcc0ddc66a01
Jay Gambetta <jay.gambetta@us.ibm.com>
2020-04-13 20:32:15 -0400

    cleaning up

---
+++ Qconfig_IBMQ_network.py
@@ -0,0 +1,3 @@
+APItoken = "a569748b77a4c2cf380e8aa6388b6b461550b0ec7e43f27cc713eecf789aed2a9a2e375dfdadc4d8df5deb86aceab651cbc29fb87182b2b2f5876536b63a4553"
+
+url = "https://q-console-api.mybluemix.net/api/Hubs/ibm-q-internal/Groups/yrk/Projects/main"
